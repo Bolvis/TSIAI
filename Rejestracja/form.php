e2a5cf1e9afee8d114777fb7ea6d1854b0b296f2
Bolvis <dawid.stasiak17@gmail.com>
2019-10-21 12:56:33 +0200

    Update form.php

--- Rejestracja/form.php
+++ Rejestracja/form.php
@@ -1,7 +1,16 @@
  <?php
- echo $_POST['surname'];
- echo $_POST['name'];
- echo $_POST['idNumber'];
+ echo '<pre>';
+echo "<h2>1.Standard</h2>";
+echo $_POST['name'];
+echo $_POST['surname'];
 
+ echo "<h2>2.var_dump</h2>";
  var_dump($_GET);
  var_dump($_POST);
+
+echo "<h2>3.print_r</h2>";
+ print_r($_POST);
+
+ echo "<h2>4.foreach echo</h2>";
+ foreach($_POST as $item) {echo "<p>$item</p>";}
+ echo '</pre>';
