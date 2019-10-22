d53309a7b2796e75ae1d3f5b44d75dc96014d316
Bolvis <dawid.stasiak17@gmail.com>
2019-10-22 02:58:53 +0200

    Update form.php

--- Rejestracja/form.php
+++ Rejestracja/form.php
@@ -1,14 +1,19 @@
  <?php
+ $file=fopen("info.txt","w") or die("Cant open file :(((");
+ $text = "";
+ foreach($_POST as $item){$text= $text.$item.",";}
+ fwrite($file,$text);
+ fclose($file);
  echo '<pre>';
-echo "<h2>1.Standard</h2>";
-echo $_POST['name'];
-echo $_POST['surname'];
+ echo "<h2>1.Standard</h2>";
+ echo $_POST['name'];
+ echo $_POST['surname'];
 
  echo "<h2>2.var_dump</h2>";
  var_dump($_GET);
  var_dump($_POST);
 
-echo "<h2>3.print_r</h2>";
+ echo "<h2>3.print_r</h2>";
  print_r($_POST);
 
  echo "<h2>4.foreach echo</h2>";
