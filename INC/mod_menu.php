c93762ab97048f53a68c1b305098731c3460c0c1
Bolvis <dawid.stasiak17@gmail.com>
2019-11-04 13:05:49 +0100

    index

--- INC/mod_menu.php
+++ INC/mod_menu.php
@@ -1,25 +1,25 @@
 menu 1
 <ul>
-    <li><a href="../index.php?id=start">start</a></li>
-    <li><a href="../index.php?id=blog">blog</a></li>
-    <li><a href="../index.php?id=contact">contact</a></li>
-    <li><a href="../index.php?id=gallery">gallery</a></li>
-    <li><a href="../index.php?id=news">news</a></li>
+    <li><a href="index.php?id=start">start</a></li>
+    <li><a href="index.php?id=blog">blog</a></li>
+    <li><a href="index.php?id=contact">contact</a></li>
+    <li><a href="index.php?id=gallery">gallery</a></li>
+    <li><a href="index.php?id=new">new</a></li>
 </ul>
 
 menu 2
 <ul>
-    <li><a href="start">start</a></li>
-    <li><a href="blog">blog</a></li>
-    <li><a href="contact">contact</a></li>
-    <li><a href="gallery">gallery</a></li>
-    <li><a href="news">news</a></li>
+    <li><a href="INC\start">start</a></li>
+    <li><a href="INC\blog">blog</a></li>
+    <li><a href="INC\contact">contact</a></li>
+    <li><a href="INC\gallery">gallery</a></li>
+    <li><a href="INC\news">news</a></li>
 </ul>
 menu 3
 <ul>
-    <li><a href="start.html">start</a></li>
-    <li><a href="blog.html">blog</a></li>
-    <li><a href="contact.html">contact</a></li>
-    <li><a href="gallery.html">gallery</a></li>
-    <li><a href="news.html">news</a></li>
+    <li><a href="INC\start.html">start</a></li>
+    <li><a href="INC\blog.html">blog</a></li>
+    <li><a href="INC\contact.html">contact</a></li>
+    <li><a href="INC\gallery.html">gallery</a></li>
+    <li><a href="INC\news.html">news</a></li>
 </ul>
--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
+
 include('INC\config.php');
 include ('INC\mod_menu.php');
+
+var_dump($_GET);
+
+$file='INC\inc_'.$_GET['id'].'.php';
+if(file_exists($file))
+{
+    include ($file);
+}
+else
+    {
+        header('Error 404 Page not found');
+    }
