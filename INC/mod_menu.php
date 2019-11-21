d968bf2b3bbca909cb0287378678e0f2dbabdd11
Bolvis <dawid.stasiak17@gmail.com>
2019-11-21 11:50:31 +0100

    Update mod_menu.php

--- INC/mod_menu.php
+++ INC/mod_menu.php
@@ -6,20 +6,3 @@ menu 1
     <li><a href="index.php?id=gallery">gallery</a></li>
     <li><a href="index.php?id=news">news</a></li>
 </ul>
-
-menu 2
-<ul>
-    <li><a href="INC\inc_start">start</a></li>
-    <li><a href="INC\inc_blog">blog</a></li>
-    <li><a href="INC\inc_contact">contact</a></li>
-    <li><a href="INC\inc_gallery">gallery</a></li>
-    <li><a href="INC\inc_news">news</a></li>
-</ul>
-menu 3
-<ul>
-    <li><a href="INC\inc_start.php">start</a></li>
-    <li><a href="inc_blog.php">blog</a></li>
-    <li><a href="INC\inc_contact.php">contact</a></li>
-    <li><a href="INC\inc_gallery.php">gallery</a></li>
-    <li><a href="INC\inc_news.php">news</a></li>
-</ul>
