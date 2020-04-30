fbe093d22a8037344ce98fb2ed33312ba937abe2
Bolvis <dawid.stasiak17@gmail.com>
2020-04-30 14:29:00 +0200

    Zadanie 18

---
+++ Zadanie 18/Osoba.php
@@ -0,0 +1,39 @@
+<?php
+
+
+class Osoba
+{
+    private $name;
+    private $surname;
+    private $sex;
+    private $birthday;
+    private $phoneNumber;
+
+    public function _construct($name, $surname,$sex,$birthday,$phoneNumber){
+        $this -> name = $name;
+        $this -> surname = $surname;
+        $this -> sex = $sex;
+        $this -> birthday = $birthday;
+        $this -> phoneNumber = $phoneNumber;
+    }
+
+    public function getInfo(){
+        echo "Osoba {name: ".$this -> name.", surname: ".$this -> surname.", sex: ".$this -> sex.", birthday: ".$this -> birthday.", Phone Number: ".$this -> phoneNumber."}";
+    }
+
+    public function getPhoneNumber(){
+        return $this -> phoneNumber;
+    }
+
+    public function setPhoneNumber($phoneNumber){
+        $this -> phoneNumber = $phoneNumber;
+    }
+
+    public function setSurname($surname){
+        $this -> surname = $surname;
+    }
+
+    public function getDays(){
+        return date_diff(date_create($this -> birthday),date('Y-m-d'));
+    }
+}
